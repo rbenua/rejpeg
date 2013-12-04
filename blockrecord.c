2bbc8bb66244fdccb9ddd6df5970601c7a691b50
cwill <c.williamso@gmail.com>
2013-12-04 16:30:40 -0500

    more block record stuff

--- blockrecord.c
+++ blockrecord.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 struct blockrecord_s {
-  int fd;
+  FILE *blob;
   size_t blocksize;
   size_t nblocks;
   size_t nheaders;
@@ -11,16 +11,16 @@ struct blockrecord_s {
   size_t curidx;
   /* Index of the next block to try */
   size_t nextidx;
+  /* Index of the block we started in for this image */
+  size_t startblock;
   /* used_blocks[i] = 1 if block i has been used */
   char *used_blocks;
-  /* offsets of headers in the file */
-  void **header_offsets;
-  /* Indiceses of blocks containing headers */
-  size_t *header_blocks;
   /* A buffer containing our current and previous buffers */
   void *block_buf;
-  /* Current point into the block */
+  /* Current point in the block buffer*/
   void *cur_offset;
+  
+  struct jpeg_decompress_struct *cinfo;
 };
 typedef struct blockrecord_s * blockrecord;
 
@@ -28,8 +28,8 @@ size_t get_idx(size_t blocksize, size_t block_offset) {
   return block_offset / blocksize;
 }
 
-blockrecord init_blockrecord(int fd, size_t nblocks, size_t blocksize,
-                             void **header_offsets, size_t nheaders) {
+blockrecord dsource_init(int fd, size_t blocksize, struct stat *statbuf
+                         struct jpeg_decompress_struct *cinfo) {
 
   blockrecord record = malloc(sizeof(blockrecord_s));
   record->fd = fd;
@@ -37,29 +37,61 @@ blockrecord init_blockrecord(int fd, size_t nblocks, size_t blocksize,
   record->nblocks = nblocks;
   record->used_blocks = calloc(sizeof(char), nblocks);
 
-  record->header_offsets = header_offsets;
-  record->header_blocks = malloc(sizeof(void *) * nblocks);
-  for (int i = 0; i < nheaders; i++) {
-    record->header_blocks[i] = get_idx(record, header_offsets[i]);
-  }
-  
   record->curidx = -1;
-  record->nextidx = header_blocks[0];
-  
-  record->block_buf = malloc(2 * blocksize);
-  record->cur_offset = 
+  record->nextidx = 0;
 
+  record->block_buf = malloc(2 * blocksize);
+  record->cur_offset = block_buf;
+  
+  record->cinfo = cinfo;
+  
   return record;
 }
 
+/* Load the next available block.  Call whenever there's not enough
+ * shit for an entire scanline. */
 void *next_block(blockrecord record) {
-  
-
-void mark_current_used(blockrecord record) {
-  used_blocks
+  record->curidx = record->nextidx;
+  /* Find the next untried, unused block. */
+  int i = curidx + 1;
+  while (record->used_blocks[i]) {
+    if (i == record->startblock) {
+      /* We ran out of blocks to try :( */
+      return NULL;
+    } else if (i >= record->nblocks - 1) {
+      /* Wrap around to the beginning.  This generally shouldn't happen unless the
+       * image is ridiculously fragmented. */
+      i = 0;
+    } else { 
+      i++;
+    }
+  }
+  record->curidx = record->nextidx;
+  record->nextidx = i;
+  /* Copy the next block into the old half of the buffer. */
+  void *dest;
+  if (record->cur_offset < (block_buf + record->blocksize))
+    dest = block_buf;
+  else
+    dest = block_buf + record->blocksize;
+  fseek(record->blob, record->curidx * record->blocksize, SEEK_SET);
+  size_t amt_read = fread(dest, 1, record->blocksize, blob);
+  /* @TODO: How do we handle EOF? */
 }
 
-void next_image(blockrecord record);
+void mark_used(blockrecord record, size_t offset) {
+  record->used_blocks[get_idx(record, offset)] = 1;
+}
+
+void mark_current_used(blockrecord record) {
+  record->used_blocs[curidx] = 1;
+}
+
+/* We've finished with the current image.  Start from a new
+ * header offset and clear tried block info. */
+void new_image(blockrecord record, size_t header_offset) {
+  
+}
 
 void free_blockrecord(blockrecord record) {
   free(record->used_blocks);
